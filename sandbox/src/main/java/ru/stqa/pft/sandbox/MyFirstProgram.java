50597e6f30b9ffd66c1dfb727b0236510e780bb9
liubovsergheeva <84126939+liubovsergheeva@users.noreply.github.com>
2021-05-25 21:47:18 +0300

    добавлены мат. операции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,11 +1,18 @@
 package ru.stqa.pft.sandbox;
 
-public class MyFirstProgram
-{
+public class MyFirstProgram {
 
-public static void main(String[] args) {
-System.out.println("Hello, world!");
-
-
-}
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+        System.out.println(1.0 / 2);
+        System.out.println("1.0" + "2");
+        System.out.println("1" + 2);
+        System.out.println((2+2)*6);
+        System.out.println("2+2=" + 2 + 2);
+        System.out.println("2+2=" + (2 + 2));
+    }
 }
