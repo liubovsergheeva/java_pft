4d8c7c2cc191e6e6752d8d49acc4e75501ff23d2
liubovsergheeva <84126939+liubovsergheeva@users.noreply.github.com>
2021-05-25 22:17:52 +0300

    добавлены операции с double и string

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
+        String somebody = "world";
+        System.out.println("Hello" + somebody + "!");
         System.out.println("Hello, world!");
         System.out.println(2+2);
         System.out.println(2*2);
@@ -14,5 +16,14 @@ public class MyFirstProgram {
         System.out.println((2+2)*6);
         System.out.println("2+2=" + 2 + 2);
         System.out.println("2+2=" + (2 + 2));
+        System.out.println("Hello"+ "world!");
+        System.out.println("площадь квадрата со стороной " + 6 + "=" + (6 * 6));
+        int l = 7;
+        int s = l * l;
+        System.out.println("площадь квадрата со стороной " + l+ "=" + s);
+        double k = 8;
+        double q = k * k;
+        System.out.println("площадь квадрата со стороной " + k+ "=" + q);
+
     }
 }
