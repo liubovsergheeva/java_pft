447c1f058d313066c58f3ce223a45e806e7b595c
liubovsergheeva <84126939+liubovsergheeva@users.noreply.github.com>
2021-05-26 13:51:52 +0300

    добавлены 2 класса и конструкторы this.l

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,12 +3,12 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        double len = 5;
-        System.out.println("площадь квадрата состороной"+len + "=" + area(len));
 
-        double a=4;
-        double b=6;
-        System.out.println("площадь прямоугольника со сторонами"+a+ "и" +b+ "=" + area(a,b));
+        Square s = new Square(5);
+        System.out.println("площадь квадрата состороной"+s.l + "=" + area(s));
+
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("площадь прямоугольника со сторонами"+r.a+ "и" +r.b+ "=" + area(r));
 
         hello("world");
         hello("user");
@@ -28,9 +28,9 @@ public class MyFirstProgram {
         System.out.println("2+2=" + (2 + 2));
         System.out.println("Hello"+ "world!");
         System.out.println("площадь квадрата со стороной " + 6 + "=" + (6 * 6));
-        int l = 7;
-        int s = l * l;
-        System.out.println("площадь квадрата со стороной " + l+ "=" + s);
+        int i = 7;
+        int ss = i * i;
+        System.out.println("площадь квадрата со стороной " + i+ "=" + ss);
         double k = 8;
         double q = k * k;
         System.out.println("площадь квадрата со стороной " + k+ "=" + q);
@@ -38,11 +38,11 @@ public class MyFirstProgram {
     public static void hello(String somebody) {
         System.out.println("hello " + somebody + " !!!!!");
     }
-    public static double area(double pi) {
-        return pi * pi;
+    public static double area(Square s) {
+        return s.l * s.l;
     }
-    public static double area (double a, double b){
-        return a * b;
+    public static double area (Rectangle r){
+        return r.a * r.b;
     }
 
 
